7d626d4719ea436e3dd76a0cbc922b32b6e830c6
wojtekmd <41647913+wojtekmd@users.noreply.github.com>
2024-01-23 12:21:28 +0100

    modify

--- routes/api.php
+++ routes/api.php
@@ -21,74 +21,80 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthenticatedController::class, 'store'])->middleware('guest')->name('login');
-Route::post('/logout', [AuthenticatedController::class, 'destroy'])->middleware('auth:api')->name('logout');
-
 Route::group([
-    'prefix' => 'pages'
+    'middleware' => ['cors']
 ], function () {
-    Route::get('', [PageController::class, 'index']);
-    Route::get('/{pageId}', [PageController::class, 'show']);
-});
 
-Route::group([
-    'prefix' => 'companies'
-], function () {
-    Route::get('/{companyId}', [CompanyController::class, 'show']);
-});
-
-Route::group([
-    'prefix' => 'partners'
-], function () {
-    Route::get('', [PartnerController::class, 'index']);
-    Route::get('/{partnerId}', [PartnerController::class, 'show']);
-});
-
-Route::group([
-    'prefix' => 'tournaments'
-], function () {
-    Route::get('/', [TournamentController::class, 'index']);
-    Route::get('/{tournamentId}', [TournamentController::class, 'show']);
-});
-
-Route::post('send-contact-form', [ContactFormController::class, 'send']);
-
-Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('/user', [MeController::class, 'user']);
+    Route::post('/login', [AuthenticatedController::class, 'store'])->middleware('guest')->name('login');
+    Route::post('/logout', [AuthenticatedController::class, 'destroy'])->middleware('auth:api')->name('logout');
 
     Route::group([
         'prefix' => 'pages'
     ], function () {
-        Route::post('', [PageController::class, 'store']);
-        Route::post('/{pageId}', [PageController::class, 'update']);
-        Route::delete('/{pageId}', [PageController::class, 'delete']);
-
-        Route::group([
-            'prefix' => '{pageId}/elements'
-        ], function () {
-            Route::post('/{pageElementId}', [PageElementController::class, 'update']);
-        });
+        Route::get('', [PageController::class, 'index']);
+        Route::get('/{pageId}', [PageController::class, 'show']);
     });
 
     Route::group([
         'prefix' => 'companies'
     ], function () {
-        Route::post('/{companyId}', [CompanyController::class, 'update']);
+        Route::get('/{companyId}', [CompanyController::class, 'show']);
     });
 
     Route::group([
         'prefix' => 'partners'
     ], function () {
-        Route::post('', [PartnerController::class, 'store']);
-        Route::patch('/{partnerId}', [PartnerController::class, 'update']);
-        Route::delete('/{partnerId}', [PartnerController::class, 'destroy']);
+        Route::get('', [PartnerController::class, 'index']);
+        Route::get('/{partnerId}', [PartnerController::class, 'show']);
     });
 
     Route::group([
         'prefix' => 'tournaments'
     ], function () {
-        Route::post('', [TournamentController::class, 'store']);
-        Route::post('/{tournamentId}', [TournamentController::class, 'update']);
-        Route::delete('/{tournamentId}', [TournamentController::class, 'delete']);
+        Route::get('/', [TournamentController::class, 'index']);
+        Route::get('/{tournamentId}', [TournamentController::class, 'show']);
     });
+
+    Route::post('send-contact-form', [ContactFormController::class, 'send']);
+
+    Route::group(['middleware' => ['auth:api']], function () {
+        Route::get('/user', [MeController::class, 'user']);
+
+        Route::group([
+            'prefix' => 'pages'
+        ], function () {
+            Route::post('', [PageController::class, 'store']);
+            Route::post('/{pageId}', [PageController::class, 'update']);
+            Route::delete('/{pageId}', [PageController::class, 'delete']);
+
+            Route::group([
+                'prefix' => '{pageId}/elements'
+            ], function () {
+                Route::post('/{pageElementId}', [PageElementController::class, 'update']);
+            });
+        });
+
+        Route::group([
+            'prefix' => 'companies'
+        ], function () {
+            Route::post('/{companyId}', [CompanyController::class, 'update']);
+        });
+
+        Route::group([
+            'prefix' => 'partners'
+        ], function () {
+            Route::post('', [PartnerController::class, 'store']);
+            Route::patch('/{partnerId}', [PartnerController::class, 'update']);
+            Route::delete('/{partnerId}', [PartnerController::class, 'destroy']);
+        });
+
+        Route::group([
+            'prefix' => 'tournaments'
+        ], function () {
+            Route::post('', [TournamentController::class, 'store']);
+            Route::post('/{tournamentId}', [TournamentController::class, 'update']);
+            Route::delete('/{tournamentId}', [TournamentController::class, 'delete']);
+        });
+    });
+
 });
